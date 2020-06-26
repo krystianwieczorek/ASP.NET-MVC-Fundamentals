d9c8ced7a4da8f1b9ab4000a8c5aabb3a0fadefc
Krystian Wieczorek <krystian.adam.wieczorek@gmail.com>
2020-06-26 14:21:54 +0200

    Added unsaved files

--- Project.Data/Services/InMemoryRestaurantData.cs
+++ Project.Data/Services/InMemoryRestaurantData.cs
@@ -1,14 +1,27 @@
 ﻿using Project.Data.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Project.Data.Services
 {
-    public class inMemoryRestaurantData : IRestaurantData
+    public class InMemoryRestaurantData : IRestaurantData
     {
+        List<Restaurant> restaurant;
+
+        public InMemoryRestaurantData()
+        {
+            restaurant = new List<Restaurant>()
+            {
+                new Restaurant{ Id = 1, Name = "Pizzaland", Cuisine = CuisineType.Italian },
+                new Restaurant{ Id = 2, Name = "Pizzaland2", Cuisine = CuisineType.Italian },
+                new Restaurant{ Id = 3, Name = "Pizzaland3", Cuisine = CuisineType.Italian },
+                new Restaurant{ Id = 4, Name = "Pizzaland4", Cuisine = CuisineType.Italian }
+            };
+        }
         public IEnumerable<Restaurant> GetAll()
         {
-            throw new NotImplementedException();
+            return restaurant.OrderBy(r => r.Name);
         }
     }
 }
